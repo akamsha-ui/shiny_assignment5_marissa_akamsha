0b5758483f14d92885d14eec6be6cad30ee05fb2
Akamsha Kurian <kurian.akamsha6@gmail.com>
2025-12-01 11:49:06 +0000

    added the dataset(mutate+select)

--- shiny_DIG/server.R
+++ shiny_DIG/server.R
@@ -7,6 +7,25 @@
 #    https://shiny.posit.co/
 #
 
+dig_new.df <- dig.df %>%
+  
+  mutate(
+    trtmt = factor(trtmt, levels = c(0,1), labels = c("Placebo", "Treatment")), 
+    sex = factor(sex, levels = c(1,2), labels = c("Males", "Females")),
+    #hyperten = factor(hyperten, levels = c(0,1)), 
+    hyperten = factor(hyperten, levels = c(0,1), labels = c("No","Yes")),
+    cvd = factor(cvd, levels = c(0,1), labels = c("No","Yes")),
+    whf = factor(whf, levels = c(0,1), labels = c("No","Yes")), 
+    dig = factor(dig, levels = c(0,1), labels = c("No","Yes")), 
+    hosp = factor(hosp, levels = c(0,1), labels = c("No","Yes")), 
+    death = factor(death, levels = c(0,1), labels = c("Alive","Death"))
+  ) %>%
+  
+  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
+
+
+
+
 library(shiny)
 
 # Define server logic required to draw a histogram

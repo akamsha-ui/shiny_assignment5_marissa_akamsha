5c9ebbe31d2e7e922190df5e6b5e80ef9d211f39
Akamsha Kurian <kurian.akamsha6@gmail.com>
2025-12-01 11:25:40 +0000

    dataset loading (mutate & select)

--- draft.R
+++ draft.R
@@ -1,5 +1,26 @@
+library(janitor)
+library(tidyverse)
+dig.df <- read.csv("DIG.csv")
+View(dig.df)
+dig.df <- dig.df %>%
+  janitor::clean_names()%>%
+  mutate(
+    trtmt = factor(trtmt, levels = c(0,1), labels = c("Placebo", "Treatment")), 
+    sex = factor(sex, levels = c(1,2), labels = c("Males", "Females")),
+    #hyperten = factor(hyperten, levels = c(0,1)), 
+    hyperten = factor(hyperten, levels = c(0,1), labels = c("No","Yes")),
+    cvd = factor(cvd, levels = c(0,1), labels = c("No","Yes")),
+    whf = factor(whf, levels = c(0,1), labels = c("No","Yes")), 
+    dig = factor(dig, levels = c(0,1), labels = c("No","Yes")), 
+    hosp = factor(hosp, levels = c(0,1), labels = c("No","Yes")), 
+    death = factor(death, levels = c(0,1), labels = c("Alive","Death"))
+  ) %>%
+  
+  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
 
+library(bslib)
 library(shiny)
+
 ui <- fluidPage(
 )
 server <- function(input, output, session) {

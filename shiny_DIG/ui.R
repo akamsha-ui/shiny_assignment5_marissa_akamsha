20ddbd247ca7a84573e8b0a7b9f9705c954af89a
Marissa <majfernandes15@gmail.com>
2025-12-02 11:11:21 +0000

    aligning

--- shiny_DIG/ui.R
+++ shiny_DIG/ui.R
@@ -5,16 +5,13 @@ library(shiny)
 library(shinydashboard)
 
 # read the DIG data set (using select for the needed data set)
-setwd("C:/Users/Marissa/Documents/1. College/1. Masters/3. HDS5105 - Statistical Computing for 
-      Biomedical Data/6. Rstudio/shiny_assignment5_marissa_akamsha/shiny_DIG")
 dig_df <- read.csv("DIG.csv")
+View(dig_df)
 digData <- dig_df %>%
   janitor::clean_names() %>% 
   select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, 
          hospdays, death, deathday)
-
-#View(dig_df)
-#View(digData)
+View(digData)
 
 #UI page layout
 ui <- dashboardPage(

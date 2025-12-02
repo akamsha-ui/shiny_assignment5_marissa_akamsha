8a14e4fd2858bb170b1644f2969669da2654dce7
Akamsha Kurian <kurian.akamsha6@gmail.com>
2025-12-02 12:30:54 +0000

    added legends section

--- shiny_DIG/ui.R
+++ shiny_DIG/ui.R
@@ -5,7 +5,7 @@ library(shiny)
 library(shinydashboard)
 
 # read the DIG data set (using select for the needed data set)
-dig_new.df <- dig_df %>%
+dig_new.df <- dig.df %>%
   janitor::clean_names() %>% 
   mutate(
     trtmt = factor(trtmt, levels = c(0,1), labels = c("Placebo", "Treatment")),
@@ -18,12 +18,7 @@ dig_new.df <- dig_df %>%
     death = factor(death, levels = c(0,1), labels = c("Alive","Death"))) %>%
   select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, 
          hospdays, death, deathday)
-<<<<<<< HEAD
-View(dig_df)
-View(digData)
-=======
 
->>>>>>> 2c0e7c78d963f39da5bf91a33922d6739047845f
 
 #UI page layout
 ui <- dashboardPage(
@@ -37,21 +32,22 @@ ui <- dashboardPage(
       menuItem("Summary Tables", tabName = "tables")
     )
   ),
- # for the dashboard layout
- dashboardBody(
-   tabItems(
-     tabItem(tabName = "info",
-             h2("DIG Trial Dashboard"),
-             verbatimTextOutput("info_para"),
-             h3("Dataset"),
-             div(style = "height:400px; overflow-y: scroll; overflow-x: scroll;", tableOutput("digds"))),
-     tabItem(tabName = "over"),
-     tabItem(tabName = "relation"),
-     tabItem(tabName = "tables")
-   )
- )
+  # for the dashboard layout
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "info",
+              h2("DIG Trial Dashboard"),
+              uiOutput("info_para"),
+              h3("Legends of the dataset"),
+              uiOutput("legends"),
+              h3("Dataset"),
+              div(style = "height:400px; overflow-y: scroll; overflow-x: scroll;", tableOutput("digds"))),
+      tabItem(tabName = "over"),
+      tabItem(tabName = "relation"),
+      tabItem(tabName = "tables")
+    )
+  )
 )
-
 
 
 

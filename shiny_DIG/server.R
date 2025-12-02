3a8d3a5e467aeb7aea0ce223277022265f41edb8
Marissa <majfernandes15@gmail.com>
2025-12-02 10:05:31 +0000

    commit

--- shiny_DIG/server.R
+++ shiny_DIG/server.R
@@ -18,23 +18,24 @@ dig.df <- read.csv("DIG.csv")
 #View(dig.df)
 dig.df <- dig.df %>%
   janitor::clean_names() %>%
-  
-  # mutate(
-  #   trtmt = factor(trtmt, levels = c(0,1), labels = c("Placebo", "Treatment")), 
-  #   sex = factor(sex, levels = c(1,2), labels = c("Males", "Females")),
-  #   #hyperten = factor(hyperten, levels = c(0,1)), 
-  #   hyperten = factor(hyperten, levels = c(0,1), labels = c("No","Yes")),
-  #   cvd = factor(cvd, levels = c(0,1), labels = c("No","Yes")),
-  #   whf = factor(whf, levels = c(0,1), labels = c("No","Yes")), 
-  #   dig = factor(dig, levels = c(0,1), labels = c("No","Yes")), 
-  #   hosp = factor(hosp, levels = c(0,1), labels = c("No","Yes")), 
-  #   death = factor(death, levels = c(0,1), labels = c("Alive","Death"))
-  # ) %>%
-  
+  mutate(
+    trtmt = factor(trtmt, levels = c(0,1), labels = c("Placebo", "Treatment")),
+    sex = factor(sex, levels = c(1,2), labels = c("Males", "Females")),
+    hyperten = factor(hyperten, levels = c(0,1), labels = c("No","Yes")),
+    cvd = factor(cvd, levels = c(0,1), labels = c("No","Yes")),
+    whf = factor(whf, levels = c(0,1), labels = c("No","Yes")),
+    dig = factor(dig, levels = c(0,1), labels = c("No","Yes")),
+    hosp = factor(hosp, levels = c(0,1), labels = c("No","Yes")),
+    death = factor(death, levels = c(0,1), labels = c("Alive","Death"))
+  ) %>%
   select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
-
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
+  output$overviewPlot <- renderPlot({
+    ggplot(digData, aes_string(x=input$variable)) +
+      geom_histogram(binwidth=1, fill="skyblue", color="black") +
+      theme_minimal() +
+      labs(title=paste("Distribution of", input$variable))
+  })
 }

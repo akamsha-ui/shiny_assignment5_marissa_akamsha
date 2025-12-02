dcafc5a68345fc662861c500635fd1afe23d3d82
Akamsha Kurian <kurian.akamsha6@gmail.com>
2025-12-02 09:47:31 +0000

    ui layout with some errors

--- shiny_DIG/ui.R
+++ shiny_DIG/ui.R
@@ -8,56 +8,41 @@
 library(tidyverse)
 library(janitor) 
 library(ggplot2)
-library(bslib)
 library(shiny)
 library(shinydashboard)
-
-
-dig.df <- read.csv("DIG.csv")
-dig.df <- dig.df %>%
-  janitor::clean_names() %>%
-  # mutate(
-  #   trtmt = factor(trtmt, levels = c(0,1), labels = c("Placebo", "Treatment")), 
-  #   sex = factor(sex, levels = c(1,2), labels = c("Males", "Females")),
-  #   #hyperten = factor(hyperten, levels = c(0,1)), 
-  #   hyperten = factor(hyperten, levels = c(0,1), labels = c("No","Yes")),
-  #   cvd = factor(cvd, levels = c(0,1), labels = c("No","Yes")),
-  #   whf = factor(whf, levels = c(0,1), labels = c("No","Yes")), 
-  #   dig = factor(dig, levels = c(0,1), labels = c("No","Yes")), 
-  #   hosp = factor(hosp, levels = c(0,1), labels = c("No","Yes")), 
-  #   death = factor(death, levels = c(0,1), labels = c("Alive","Death"))
-  # ) %>%
-  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
-
-# Define UI for application that draws a histogram
-#dashboardHeader(title = "Digoxin Toxicity Trial Analysis")
-dashboardPage(skin = "purple",
-  dashboardHeader(title = "Digoxin Toxicity Trial Analysis"),
+# read the DIG data set (using select for the needed data set)
+#setwd("C:/Users/ak/Documents/my_git_hub_assignment/final_a5/shiny_assignment5_marissa_akamsha/shiny_DIG")
+dig_df <- read.csv("DIG.csv")
+digData <- dig_df %>%
+  janitor::clean_names() %>% 
+  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, 
+         hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
+#View(dig_df)
+#View(digData)
+#UI page layout
+ui <- dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = "DIG Trial Dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("About the dataset", tabName = "about", icon = icon("dashboard")),
-      menuItem("Analysis", tabName = "analysis", icon = icon("th"))
-    )),
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                box(title = "Controls",
-                    sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
-              )
-      ),
-      
-      # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
-      )
+      menuItem("info", tabName = "About the data set"),
+      menuItem("over", tabName = "Overview"),
+      menuItem("Analysis for two variables", tabName = "relationships"),
+      menuItem("Summary Tables", tabName = "tables")
     )
-  )
-  
+  ),
+ # for the dashboard layout
+   dashboardBody(
+    tabItems(
+      tabItem(tabName = "info",
+              h2("DIG Trial Dashboard"),
+              p("Explore key aspects and relationships in the DIG Trial dataset.")
+      ),
+    )
 )
+)
+
+
 
 
 

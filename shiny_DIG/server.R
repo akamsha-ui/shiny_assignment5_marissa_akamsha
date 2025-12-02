09605041b1d4667153faf4159e5800e4aee094b4
Marissa <majfernandes15@gmail.com>
2025-12-02 10:42:10 +0000

    rendering dig dataset on info page

--- shiny_DIG/server.R
+++ shiny_DIG/server.R
@@ -30,12 +30,51 @@ dig.df <- dig.df %>%
   ) %>%
   select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
 
+setwd("C:/Users/Marissa/Documents/1. College/1. Masters/3. HDS5105 - Statistical Computing for Biomedical Data/6. Rstudio/shiny_assignment5_marissa_akamsha/shiny_DIG")
+dig_df <- read.csv("DIG.csv")
+digData <- dig_df %>%
+  janitor::clean_names() %>% 
+  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
+
+
+para1 = ("The DIG (Digitalis Investigation Group) Trial was a randomized, double-blind, multicenter trial with more than 300 centers in the United States and Canada participating. The purpose of the trial was to examine the safety and efficacy of Digoxin in treating patients with congestive heart failure in sinus rhythm. Digitalis was introduced clinically more than 200 years ago and has since become a commonly prescribed medication for the treatment of heart failure; however, there was considerable uncertainty surrounding its safety and efficacy. Small trials indicated that Digoxin alleviated some of the symptoms of heart failure, prolonged exercise tolerance, and generally improved the quality of patients' lives. Unfortunately, these trials were generally small and although they did focus on the effect of treatment on patients’ relief from heart failure symptoms and quality of life, they failed to address the effect of treatment on cardiovascular outcomes. Questions about the safety of Digoxin were also a concern. Digoxin toxicity is uncommon in small trials with careful surveillance, however, the long-term effects of therapeutic levels of Digoxin were less clear.")
+para2 = ("The DIG dataset consists of baseline and outcome data from the main DIG trial. In the main trial, heart failure patients meeting the eligibility criterion and whose ejection fraction was 45% or less were randomized to receive either a placebo or digoxin. Outcomes assessed in the trial included: cardiovascular mortality, hospitalization or death from worsening heart failure, hospitalization due to other cardiovascular causes and hospitalization due to non-cardiovascular causes.")
+para3 = ("It was obtained for the purpose of this assignment and is enclosed with this assignment. The codebook associated with the variables is also enclosed with your assignment.")
+para4 = ("In order to create an anonymous dataset that protects patient confidentiality, most variables have been permuted over the set of patients within treatment group. Therefore, it would be inappropriate to use this dataset for other research or publication purposes.")
+rev = paste(para1, para2, para3, para4, sep = "<p>")
+rev
+
 # Define server logic required to draw a histogram
 function(input, output, session) {
-  output$overviewPlot <- renderPlot({
-    ggplot(digData, aes_string(x=input$variable)) +
-      geom_histogram(binwidth=1, fill="skyblue", color="black") +
-      theme_minimal() +
-      labs(title=paste("Distribution of", input$variable))
+  # paras <- paste(para1, para2, para3, para4, sep = "\n")
+  # output$info_para <- renderText(paras)
+  
+  output$info_para <- renderText({
+    HTML("<p>The DIG (Digitalis Investigation Group) Trial was a randomized, double-blind, 
+         multicenter trial with more than 300 centers in the United States and Canada participating. 
+         The purpose of the trial was to examine the safety and efficacy of Digoxin in treating patients 
+         with congestive heart failure in sinus rhythm. Digitalis was introduced clinically more than 200 
+         years ago and has since become a commonly prescribed medication for the treatment of heart failure; 
+         however, there was considerable uncertainty surrounding its safety and efficacy. Small trials 
+         indicated that Digoxin alleviated some of the symptoms of heart failure, prolonged exercise 
+         tolerance, and generally improved the quality of patients' lives. Unfortunately, these trials were
+         generally small and although they did focus on the effect of treatment on patients’ relief from 
+         heart failure symptoms and quality of life, they failed to address the effect of treatment on 
+         cardiovascular outcomes. Questions about the safety of Digoxin were also a concern. Digoxin 
+         toxicity is uncommon in small trials with careful surveillance, however, the long-term effects 
+         of therapeutic levels of Digoxin were less clear.<p>
+         <p>The DIG dataset consists of baseline and outcome data from the main DIG trial. In the 
+         main trial, heart failure patients meeting the eligibility criterion and whose ejection 
+         fraction was 45% or less were randomized to receive either a placebo or digoxin. Outcomes 
+         assessed in the trial included: cardiovascular mortality, hospitalization or death from 
+         worsening heart failure, hospitalization due to other cardiovascular causes and 
+         hospitalization due to non-cardiovascular causes.<p>
+         <p>It was obtained for the purpose of this assignment and is enclosed with this assignment. 
+         The codebook associated with the variables is also enclosed with your assignment.<p>
+         <p>In order to create an anonymous dataset that protects patient confidentiality, most 
+         variables have been permuted over the set of patients within treatment group. 
+         Therefore, it would be inappropriate to use this dataset for other research or 
+         publication purposes.<p>")
   })
-}
+  }
+
--- shiny_DIG/ui.R
+++ shiny_DIG/ui.R
@@ -1,25 +1,21 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
+
 library(tidyverse)
 library(janitor) 
 library(ggplot2)
 library(shiny)
 library(shinydashboard)
+
 # read the DIG data set (using select for the needed data set)
-#setwd("C:/Users/ak/Documents/my_git_hub_assignment/final_a5/shiny_assignment5_marissa_akamsha/shiny_DIG")
+setwd("C:/Users/Marissa/Documents/1. College/1. Masters/3. HDS5105 - Statistical Computing for Biomedical Data/6. Rstudio/shiny_assignment5_marissa_akamsha/shiny_DIG")
 dig_df <- read.csv("DIG.csv")
 digData <- dig_df %>%
   janitor::clean_names() %>% 
-  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, 
-         hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
+  select(id, trtmt, age, sex, bmi, klevel, creat, diabp, sysbp, hyperten, cvd, whf, dig, hosp, hospdays, death, deathday)
 
 
 #about df for the info page
+
+
 #View(dig_df)
 #View(digData)
 #UI page layout
@@ -39,25 +35,13 @@ ui <- dashboardPage(
    tabItems(
      tabItem(tabName = "info",
              h2("DIG Trial Dashboard"),
-             paste("
-
-\tThe DIG dataset consists of baseline and outcome data from the main DIG trial. In the main
-trial, heart failure patients meeting the eligibility criterion and whose ejection fraction was 45%
-or less were randomized to receive either a placebo or digoxin. Outcomes assessed in the trial
-included: cardiovascular mortality, hospitalization or death from worsening heart failure,
-hospitalization due to other cardiovascular causes and hospitalization due to non-cardiovascular
-causes.
-
-The **DIG** dataset was obtained for the purpose of this assignment and is enclosed with this assignment. The codebook associated with the variables is also enclosed with your assignment.
-
-
-In order to create an anonymous dataset that protects patient confidentiality, most variables have been permuted over the set of patients within treatment group. Therefore, it would be inappropriate to use this dataset for other research or publication purposes. ")
-     ),
+             uiOutput("info_para"),
      tabItem(tabName = "over"),
      tabItem(tabName = "relation"),
      tabItem(tabName = "tables")
    )
  )
+)
 )
 
 
